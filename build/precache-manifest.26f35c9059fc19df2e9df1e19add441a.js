48f3c11784b397dbebe0e875805565930a424a37
Vicente Antonio G. Reyes <highcenoid@Highcenburg.local>
2020-03-17 08:25:31 +0800

    pre-deploy

---
+++ build/precache-manifest.26f35c9059fc19df2e9df1e19add441a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d496d7632e18f2e329502b3bedc082",
+    "url": "/intro-react/index.html"
+  },
+  {
+    "revision": "c258e374088a250e4b51",
+    "url": "/intro-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/intro-react/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "c258e374088a250e4b51",
+    "url": "/intro-react/static/js/main.7ad2de86.chunk.js"
+  },
+  {
+    "revision": "aa8e1f74143b90d1364a",
+    "url": "/intro-react/static/js/runtime~main.37ae2610.js"
+  }
+]);
